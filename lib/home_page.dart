cf5d761e85a8e23db6cf976f8029210de84b33bc
HiloongHo <hiloongho@gmail.com>
2025-01-13 23:29:13 +0800

    finished!

---
+++ lib/home_page.dart
@@ -0,0 +1,56 @@
+import 'package:faker/faker.dart';
+import 'package:flutter/material.dart';
+
+import 'order_data_table.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final faker = Faker();
+
+  @override
+  void initState() {
+    super.initState();
+    setNewCustomer();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Orders App'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.person_add_alt),
+            onPressed: setNewCustomer,
+          ),
+          IconButton(
+            icon: const Icon(Icons.attach_money),
+            onPressed: addFakeOrderForCurrentCustomer,
+          ),
+        ],
+      ),
+      body: OrderDataTable(
+        // TODO: Pass in the orders
+        onSort: (columnIndex, ascending) {
+          // TODO: Query the database and sort the data
+        },
+      ),
+    );
+  }
+
+  void setNewCustomer() {
+    // TODO: Implement properly
+    print('Name: ${faker.person.name()}');
+  }
+
+  void addFakeOrderForCurrentCustomer() {
+    // TODO: Implement properly
+    print('Price: ${faker.randomGenerator.integer(500, min: 10)}');
+  }
+}
